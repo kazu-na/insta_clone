5c75875731d283a8e8d9a292dbddcc38dfa51668
kazu_na <kazuhide.n428@gmail.com>
2019-10-22 11:39:02 +0900

    Add facebook2

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get    '/login',                      to: 'sessions#new'
   post   '/login',                      to: 'sessions#create'
   delete '/logout',                     to: 'sessions#destroy'
-  get    '/auth/:provider/callback',    to: 'users#create',          as: :auth_callback
+  get    '/auth/facebook/callback',     to: 'users#create',          as: :auth_callback
   get    '/auth/failure',               to: 'users#auth_failure',    as: :auth_failure
 
   resources :users do
